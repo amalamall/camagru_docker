16d367d29c91fc58b84a41a96dae7c4747c236a2
amal <amal@MacBook-de-zaki.local>
2020-10-24 17:52:41 +0100

    ya rebi

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'mrtam');
+  define('DB_PASS', '');
+  define('DB_NAME', 'camagru');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://165.227.175.72/camagru');
+  // Site Name
+  define('SITENAME', 'CAMAGRU');
+  // App Version
+  define('APPVERSION', '1.0.0');
